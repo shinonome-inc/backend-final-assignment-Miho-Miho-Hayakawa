2fd4e78658c0e94a14d769c26cd08ad100532a89
Maco <Maco_hakumo@protonmail.com>
2022-12-17 16:56:31 +0900

    サインアップ機能

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,42 +1,161 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth import SESSION_KEY
+
+
+from .models import User
 
 
 class TestSignUpView(TestCase):
     def test_success_get(self):
-        pass
+        res = self.client.get(reverse('accounts:signup'))
+        self.assertEqual(res.status_code, 200)
 
     def test_success_post(self):
-        pass
+        data = {
+                "username": "test",
+                "email": "test@example.com",
+                "password1": "examplepassword",
+                "password2": "examplepassword",
+                }
+        res = self.client.post(reverse('accounts:signup'), data)
+        self.assertRedirects(
+                res,
+                reverse('tweets:home'),
+                status_code=302,
+                target_status_code=200,
+                )
+        self.assertEqual(User.objects.all().count(), 1)
+        self.assertEqual(
+                User.objects.filter(
+                    username='test', email='test@example.com'
+                    ).count(),
+                1,
+                )
+        self.assertIn(SESSION_KEY, self.client.session)
 
     def test_failure_post_with_empty_form(self):
-        pass
+        data = {
+                'username': '',
+                'email': '',
+                'password1': '',
+                'password2': '',
+                }
+        response = self.client.post(reverse('accounts:signup'), data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.all().count(), 0)
+        self.assertFormError(response, "form", "username", ["このフィールドは必須です。"])
+        self.assertFormError(response, "form", "email", ["このフィールドは必須です。"])
+        self.assertFormError(response, "form", "password1", ["このフィールドは必須です。"])
+        self.assertFormError(response, "form", "password2", ["このフィールドは必須です。"])
 
     def test_failure_post_with_empty_username(self):
-        pass
+        data = {
+                'username': '',
+                'email': 'test@example.com',
+                'password1': 'abcd1234',
+                'password2': 'abcd1234',
+                }
+        response = self.client.post(reverse('accounts:signup'), data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.all().count(), 0)
+        self.assertFormError(response, "form", "username", ["このフィールドは必須です。"])
 
     def test_failure_post_with_empty_email(self):
-        pass
+        data = {
+                'username': 'test',
+                'email': '',
+                'password1': 'abcd1234',
+                'password2': 'abcd1234',
+                }
+        response = self.client.post(reverse('accounts:signup'), data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.all(). count(), 0)
+        self.assertFormError(response, "form", "email", ['このフィールドは必須です。'])
 
     def test_failure_post_with_empty_password(self):
-        pass
+        data = {
+                'username': 'test',
+                'email': 'test@example.com',
+                'password1': '',
+                'password2': '',
+                }
+        response = self.client.post(reverse('accounts:signup'), data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.all(). count(), 0)
+        self.assertFormError(response, "form", "password1", "このフィールドは必須です。")
+        self.assertFormError(response, "form", "password2", "このフィールドは必須です。")
 
     def test_failure_post_with_duplicated_user(self):
-        pass
+        User.objects.create_user(
+                username='test',
+                email='test@example.com',
+                password='abcd1234',
+                )
+        data = {
+                'username': 'test',
+                'email': 'test@example.com',
+                'password1': 'abcd1234',
+                'password2': 'abcd1234',
+                }
+        response = self.client.post(reverse('accounts:signup'), data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.all().count(), 1)
 
     def test_failure_post_with_invalid_email(self):
-        pass
+        data = {
+                'username': 'test',
+                'email': 'test',
+                'password1': 'abcd1234',
+                'password2': 'abcd1234',
+                }
+        response = self.client.post(reverse('accounts:signup'), data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.all().count(), 0)
 
     def test_failure_post_with_too_short_password(self):
-        pass
+        data = {
+                'username': 'test',
+                'email': 'test@example.com',
+                'password1': 'abcd',
+                'password2': 'abcd',
+                }
+        response = self.client.post(reverse('accounts:signup'), data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.all().count(), 0)
 
     def test_failure_post_with_password_similar_to_username(self):
-        pass
+        data = {
+                'username': 'test',
+                'email': 'test@example.com',
+                'password1': 'test',
+                'password2': 'test',
+                }
+        response = self.client.post(reverse('accounts:signup'), data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.all().count(), 0)
 
     def test_failure_post_with_only_numbers_password(self):
-        pass
+        data = {
+                'username': 'test',
+                'email': 'test@example.com',
+                'password1': '271828',
+                'password2': '271828',
+                }
+        response = self.client.post(reverse('accounts:signup'), data=data)
+        self.assertEqual(response.status_code, 200)
+ #       self.assertEqual(response.objects.all().count(), 0)
 
     def test_failure_post_with_mismatch_password(self):
-        pass
+        data = {
+                'username': 'test',
+                'email': 'test@example.com',
+                'password1': 'abcd1234',
+                'password2': '1234abcd',
+                }
+        response = self.client.post(reverse('accounts:signup'), data=data)
+        self.assertEqual(response.status_code, 200)
+#        self.assertEqual(response.objects.all().count(), 0)
 
 
 class TestLoginView(TestCase):
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import User
+from .forms import SignUpForm
+from django.contrib.auth import authenticate, login
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class SignUpView(generic.CreateView):
+    form_class = SignUpForm
+    template_name = "accounts/signup.html"
+    model = User
+    success_url = reverse_lazy('tweets:home')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password1")
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return response
